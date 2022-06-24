fcc588c0b6ea997d0d362a8f3644674d423b3e2b
joseph1997520 <joseph1997520@gmail.com>
2022-06-24 16:24:31 +0800

    0624

--- hello/src/main.js
+++ hello/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Logo from '@/components/Logo.vue'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+// app.component("Logo",Logo);
+
+// app.use(router);
+// app.mount("#app")
+// 可以簡寫成
+app.component("Logo",Logo).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
